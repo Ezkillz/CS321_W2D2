54439d0e4f40ddbe7589041d3d1dd20c4fb7483f
Ezkillz <47401914+Ezkillz@users.noreply.github.com>
2019-11-03 12:51:49 -0600

    Just Created a repository for this file and Finished project.

---
+++ CS321_W2D2_StudentAPI/CS321_W2D2_StudentAPI/Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations; // Just addded this
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CS321_W2D2_StudentAPI.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "FistName cannot be empty")]
+        
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(75, ErrorMessage = "FirstName cannot be more than 75 char")]
+        public string LastName { get; set; }
+        [Required]
+        [MaxLength(75, ErrorMessage = "FirstName cannot be more than 75 char")]
+        public DateTime BirthDate { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public string Email { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Phone { get; set; }
+        [Required]
+        [Phone]
+
+    }
+}
---
+++ CS321_W2D2_StudentAPI/CS321_W2D2_StudentAPI/Services/StudentService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks; 
+using CS321_W2D2_StudentAPI.Models;
+
+namespace CS321_W2D2_StudentAPI.Services
+{
+    public class StudentService: IStudentsService
+    {
+        private List<Student> _student = new List<Student>()
+        {
+            new Student
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName= "Doe",
+                BirthDate = new DateTime(2010,1,1),
+                Email ="john.doe@test.com",
+                Phone = "555.555.5555"
+            },
+            new Student
+            {
+                Id = 2,
+                FirstName = "Jane",
+                LastName = "Smith",
+                BirthDate= new DateTime(2012,1,1),
+                Email = "jane.smith@test.com",
+                Phone = "555.555.5555"
+            }
+        };
+        private int _nextId = 3;
+
+        // implementations of IStudentServices  
+        public IEnumerable<Student> GetAll()
+        {
+            return _student;
+        }
+        // Get(int id)
+        public Student Get(int id)
+        {
+            var student = _student.FirstOrDefault(s => s.Id == id);
+            return student;
+        }
+        // Add (Student student)
+        public Student Add(Student student)
+        {
+            ValidateBithdate(student);
+            student.Id = _nextId++;
+            _student.Add(student);
+            return student;
+        }
+        // Checking Age/ DateOfBirth
+        public void ValidateBithdate(Student student)
+        {
+            if(student.BirthDate > DateTime.Now)
+            {
+                throw new Exception("Birthdate cant be in future!");
+            }
+            if(DateTime.Now.Year - student.BirthDate.Year>18)
+            {
+                throw new Exception("You are ")
+            }
+        }
+        //Update
+        public Student Update(Student student)
+        {
+            var currentStudent = this.Get(student.Id); // student.FirstOrDefault(s => s.Id == id)
+            if (currentStudent == null) return null;
+            currentStudent.FirstName = student.FirstName;
+            currentStudent.LastName = student.LastName;
+            currentStudent.BirthDate = student.BirthDate;
+            currentStudent.Email = student.Email;
+            currentStudent.Phone = student.Phone;
+            return currentStudent;
+        }
+        // Remove
+        public void Remove(Student student)
+        {
+            _student.Remove(student);
+        }
+    }
+}
